5b2e4e832d353fbd142d362a84ddded06d5aac5f
Chamali Gorakanage <gora0021@algonquinlive.com>
2021-07-27 16:46:36 -0400

    Before loading news 2021.07.27

--- app/src/main/java/algonquin/cst2335/finalproject/NewsArticles.java
+++ app/src/main/java/algonquin/cst2335/finalproject/NewsArticles.java
@@ -1,14 +1,22 @@
 package algonquin.cst2335.finalproject;
 
+import android.app.AlertDialog;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,12 +29,15 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class NewsArticles extends AppCompatActivity {
+    ArrayList<newsFeed> news = new ArrayList<>();
+    MyNewsAdapter adt = new MyNewsAdapter();
+
     private String stringURL;
     RecyclerView newsList;
 
@@ -40,8 +51,28 @@ public class NewsArticles extends AppCompatActivity {
 
         setContentView(R.layout.newslayout);
 
+        //--------------
+
+        String newsTitle1="Olympic soccer standings 2021";
+        String newsDate ="21-Jul-2021";
+        ImageView newsImage = findViewById(R.id.newsimage);
+
+        Button save = findViewById(R.id.savebutton);
+        Button load = findViewById(R.id.loadbutton);
+        RecyclerView newsList = findViewById(R.id.myrecycler);
+
+        newsList.setAdapter(adt);
 
 
+        newsList.setLayoutManager(new LinearLayoutManager(this));
+
+
+        //newsFeed thisNews = new newsFeed(newsTitle1 ,newsDate,newsImage);
+        newsFeed thisNews = new newsFeed(newsTitle1 ,newsDate,newsImage);
+        news.add( thisNews );
+        adt.notifyItemInserted(news.size()-1);
+
+        //-------------
 
         Executor newThread = Executors.newSingleThreadExecutor();
         newThread.execute( () ->{
@@ -88,12 +119,102 @@ public class NewsArticles extends AppCompatActivity {
         });
     }
 
+    //--------MyRowView
+
+    private class MyRowViews extends RecyclerView.ViewHolder{
+        TextView newsTitle;
+        TextView newsDate;
+        Image newsImage;
+        int position = -1;
+
+        public MyRowViews(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener( click -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder( NewsArticles.this );
+                builder.setMessage("Do you want to save the news:" + newsTitle.getText())
+                        .setTitle("Question")
+                        .setNegativeButton("No",(dialog, cl) ->{ })
+                        .setPositiveButton("Yes",(dialog, cl) ->{
+
+                            position=getAbsoluteAdapterPosition();
+
+                            newsFeed saveNews = news.get(position); //Keep the delted message text to undo if needed
+                            //news.remove(position);
+                            //adt.notifyItemRemoved(position);
+
+                            Snackbar.make(newsTitle,"You saved news #" + position, Snackbar.LENGTH_LONG)
+                                    .setAction("Undo", clk -> {
+                                        //Undo the delete
+                                        news.add(position,saveNews);
+                                        adt.notifyItemInserted(position);
+                                    })
+                                    .show();
+                        })
+                        .create().show();
+            });
+
+            newsTitle = itemView.findViewById(R.id.message);
+            newsDate  = itemView.findViewById(R.id.time);
+        }
+
+        public void setPosition(int p) { position=p; }
+    }
+
+    //-------------
+
+    private class MyNewsAdapter extends RecyclerView.Adapter{
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = getLayoutInflater();
+            int layoutID;
+
+            layoutID=R.layout.load_news;
+
+            View loadedRow = inflater.inflate(layoutID, parent, false);
+            MyRowViews initRow = new MyRowViews(loadedRow);
+
+            return initRow;
+        }
+
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            MyRowViews thisView = (MyRowViews)holder;
+            thisView.newsTitle.setText(news.get(position).getTitle());
+            thisView.newsDate.setText(news.get(position).getNewsDate());
+            thisView.setPosition(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            //Numbwer of items in array
+            return news.size();
+        }
+
+
+
+    }
+
+    //------
 
     private class newsFeed
     {
-        String title;
-        Date newsDate;
-        Image newsImage;
+        public String title;
+        public String newsDate;
+        public ImageView newsImage;
+
+        public newsFeed(String title, String newsDate, ImageView newsImage){
+            this.title = title;
+            this.newsDate = newsDate;
+            this.newsImage = newsImage;
+        }
+
+        public String getTitle(){return  title;}
+        public String getNewsDate(){return newsDate;}
+        public ImageView getNewsImage(){return newsImage;}
     }
+
+
 
 }
