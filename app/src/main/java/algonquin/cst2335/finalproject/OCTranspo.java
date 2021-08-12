684cc9570479aa0d08c730eef7e8eb9d659b6dd0
Cody Varrette <varr0011@algonquincollege.com>
2021-08-12 12:15:02 -0400

    First Commit - Needs Javadoc + JSON fix

---
+++ app/src/main/java/algonquin/cst2335/finalproject/OCTranspo.java
@@ -0,0 +1,241 @@
+package algonquin.cst2335.finalproject;
+
+import android.app.AlertDialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+public class OCTranspo extends AppCompatActivity {
+
+    RecyclerView busList;
+    ArrayList<busInfo> busses = new ArrayList<>();
+    MyChatAdapter adt = new MyChatAdapter();
+
+    String stringURL;
+    String stringURL2;
+
+    JSONObject tripDest;
+    JSONObject latit;
+    JSONObject longi;
+    JSONObject gpsS;
+    JSONObject startT;
+    JSONObject schedAdj;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.octranspo);
+
+        busList = findViewById(R.id.results);
+        busList.setAdapter(adt);
+        busList.setLayoutManager(new LinearLayoutManager(this));
+
+        Button enterButton = findViewById(R.id.search_button);
+        EditText enterStop = findViewById(R.id.busRouteNumber);
+        EditText enterBus = findViewById(R.id.busNumber);
+
+        enterButton.setOnClickListener( clk -> {
+            Executor newThread = Executors.newSingleThreadExecutor();
+            newThread.execute( () -> {
+                String enterStopNum = enterStop.getText().toString();
+                try {
+
+                    stringURL = "https://api.octranspo1.com/v2.0/GetRouteSummaryForStop?appID=223eb5c3&&apiKey=ab27db5b435b8c8819ffb8095328e775&stopNo="
+                            + URLEncoder.encode(enterStopNum, "UTF-8")
+                            + "&format=json";
+                    URL url = new URL(stringURL);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                    String text = (new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8)))
+                            .lines()
+                            .collect(Collectors.joining("\n"));
+                    JSONObject theDocument = new JSONObject(text);
+                } catch (IOException | JSONException ioe) {
+                    Log.e("Connection error", ioe.getMessage());
+                }
+                try {
+                    String enterBusNum = enterBus.getText().toString();
+
+                    stringURL2 = "https://api.octranspo1.com/v2.0/GetNextTripsForStop?appID=223eb5c3&&apiKey=ab27db5b435b8c8819ffb8095328e775&stopNo="
+                            + URLEncoder.encode(enterStopNum, "UTF-8")
+                            +"&routeNO="
+                            +URLEncoder.encode(enterBusNum, "UTF-8")
+                            + "&format=json";
+                    URL url2 = new URL(stringURL2);
+                    HttpURLConnection urlConnection2 = (HttpURLConnection) url2.openConnection();
+                    InputStream in2 = new BufferedInputStream(urlConnection2.getInputStream());
+
+                    String text2 = (new BufferedReader(new InputStreamReader(in2, StandardCharsets.UTF_8)))
+                            .lines()
+                            .collect(Collectors.joining("\n"));
+                    JSONObject theDocument2 = new JSONObject( text2 );
+                    tripDest = theDocument2.getJSONObject("TripDestination");
+                }
+                catch (IOException | JSONException ioe){
+
+                }
+            });
+
+            busInfo thisRoute = new busInfo(enterStop.getText().toString(), "destination" , "lat", "long", "gps", "start", "sch");
+            busses.add(thisRoute);
+            adt.notifyItemInserted(busses.size()-1);
+        });
+    }
+
+    private class MyRowViews extends RecyclerView.ViewHolder{
+
+        TextView numberText;
+        TextView destinationText;
+        TextView latText;
+        TextView longText;
+        TextView gpsText;
+        TextView startTimeText;
+        TextView scheduleText;
+        int position = - 1;
+
+        public MyRowViews(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener( click -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder( OCTranspo.this );
+                builder.setMessage("Do you want to delete this result for " + numberText.getText() + "?")
+                        .setTitle("Question:")
+                        .setNegativeButton("No", (dialog, cl) -> { })
+                        .setPositiveButton("Yes", (dialog, cl) -> {
+
+                            position = getAdapterPosition();
+
+                            busInfo removedResult = busses.get(position);
+                            busses.remove(position);
+                            adt.notifyItemRemoved(position);
+
+                            Snackbar.make(numberText, "You deleted result #" + position, Snackbar.LENGTH_LONG)
+                                    .setAction("Undo", clk -> {
+                                        busses.add(position, removedResult);
+                                        adt.notifyItemInserted(position);
+                                    })
+                                    .show();
+                        })
+                        .create().show();
+
+
+            });
+
+            numberText = itemView.findViewById(R.id.stopNumberView);
+            destinationText = itemView.findViewById(R.id.destinationView);
+            latText = itemView.findViewById(R.id.latitudeView);
+            longText = itemView.findViewById(R.id.longitudeView);
+            gpsText = itemView.findViewById(R.id.gpsView);
+            startTimeText = itemView.findViewById(R.id.startView);
+            scheduleText = itemView.findViewById(R.id.scheduleTimeView);
+        }
+
+        public void setPosition(int p) { position = p; }
+    }
+
+    private class MyChatAdapter extends RecyclerView.Adapter<MyRowViews> {
+        @Override
+        public MyRowViews onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MyRowViews(getLayoutInflater().inflate(R.layout.busrouteinfo, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(MyRowViews holder, int position) {
+            holder.numberText.setText(busses.get(position).getStopNum());
+            holder.destinationText.setText(busses.get(position).getDestination());
+            holder.latText.setText(busses.get(position).getLatitude());
+            holder.longText.setText(busses.get(position).getLongitude());
+            holder.gpsText.setText(busses.get(position).getSpeed());
+            holder.startTimeText.setText(busses.get(position).getTime());
+            holder.scheduleText.setText(busses.get(position).getSchedule());
+            holder.setPosition(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return busses.size();
+        }
+    }
+
+    private class busInfo {
+        String stopNum;
+        String destination;
+        String latitude;
+        String longitude;
+        String speed;
+        String time;
+        String schedule;
+
+        public busInfo(String stopNum, String destination, String latitude, String longitude, String speed, String time, String schedule) {
+            this.stopNum = stopNum;
+            this.destination = destination;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.speed = speed;
+            this.time = time;
+            this.schedule = schedule;
+        }
+
+        public String getStopNum() {
+            return stopNum;
+        }
+
+        public String getDestination() {
+            return destination;
+        }
+
+        public String getLatitude() {
+            return latitude;
+        }
+
+        public String getLongitude() {
+            return longitude;
+        }
+
+        public String getSpeed() {
+            return speed;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public String getSchedule() {
+            return schedule;
+        }
+    }
+}
